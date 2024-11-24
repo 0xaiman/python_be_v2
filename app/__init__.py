b6648b48485e6d10c1aa26e58d1e06bb45fd0d2a
0xaiman <aimandani9797@gmail.com>
2024-11-25 02:21:24 +0800

    feature:rate-limiter when creating parking session , max 50 request per minute

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,29 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import SQLAlchemy
 
-from app.api.v1.routes import api as v1_api
+from app.api.v1.routes import \
+    create_v1_routes  # Ensure this is the correct import
 from app.api.v2.routes import api as v2_api
 from app.db import db
-from app.models import parking_logs
 
 
 def create_app():
     app = Flask(__name__)
-    
+
+    # Initialize Limiter
+    limiter = Limiter(
+        get_remote_address,  # Use IP address to track requests
+        app=app,  # Bind the limiter to the Flask app
+        default_limits=["200 per day", "50 per hour"],  # Default rate limits
+        storage_uri="memory://",  # In-memory storage for rate limits (ideal for dev, not for prod)
+    )
 
     # Register API blueprints
-    app.register_blueprint(v1_api, url_prefix='/api/v1')
+    app.register_blueprint(create_v1_routes(limiter), url_prefix='/api/v1')
     app.register_blueprint(v2_api, url_prefix='/api/v2')
 
     # Database configuration
@@ -23,10 +32,17 @@ def create_app():
 
     db.init_app(app)
 
+    @app.errorhandler(429)
+    def rate_limit_error(error):
+        return jsonify({
+            "error": "Too Many Requests",
+            "message": "You have exceeded the rate limit. Please try again later.",
+            "status_code": 429
+        }), 429
+
     @app.cli.command('init-db')
     def init_db():
         db.create_all()
         print("Database initialized!")
-
 
     return app
--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -5,7 +5,6 @@ import os
 from datetime import datetime
 
 import requests
-from dotenv import load_dotenv
 from flask import Blueprint, jsonify, request
 from PIL import Image, ImageDraw, ImageFont
 
@@ -14,89 +13,87 @@ from app.models.parking_logs import ParkingLog
 
 api = Blueprint('v1', __name__)
 
+from dotenv import load_dotenv
+
 load_dotenv()
 
-
-@api.route('/test', methods=['POST'])
-def test():
-    return jsonify({"message": "This is a test endpoint for v1, MILESIGHT CAMERA."}), 200
- 
-
-@api.route('/parking-session/operatorId/<int:operatorId>/floorId/<int:floorId>', methods=['POST'])
-# /parking-session/operatorId/1/floorId/1 GROUND (100)
-# /parking-session/operatorId/1/floorId/2 LEVEL 1 (100)
-# ...
-# /parking-session/operatorId/1/floorId/10 LEVEL10
-# 
-def parking_event(operatorId, floorId):
-    try:
-        raw_data = request.json
-        bearer_token = os.getenv("BEARER_TOKEN")
-        if not bearer_token:
-            return jsonify({"error": "Bearer token not found in .env file"}), 500
+def create_v1_routes(limiter):
+    @api.route('/test', methods=['POST'])
+    @limiter.limit("5 per minute")
+    def test():
+        return jsonify({"message": "This is a test endpoint for v1, MILESIGHT CAMERA."}), 200
     
-        headers = {
-        "Authorization": f"Bearer {bearer_token}",
-        "Content-Type": "application/json"
-    }
+
+    @api.route('/parking-session/operatorId/<int:operatorId>/floorId/<int:floorId>', methods=['POST'])
+    @limiter.limit("5 per minute")
+    def parking_event(operatorId, floorId):
+        try:
+            raw_data = request.json
+            bearer_token = os.getenv("BEARER_TOKEN")
+            if not bearer_token:
+                return jsonify({"error": "Bearer token not found in .env file"}), 500
         
-        base_url = os.getenv("TARGET_URL","http://localhost:3000/parking-session/operatorId/1/floorId/1")
-        print("operator id:",operatorId)
-        print("floor id:",floorId)
-        print("raw data", raw_data)
-
-        # 1 -Generate target url to NESTJS
-        target_url = f"{base_url}/parking-session/operatorId/{operatorId}/floorId/{floorId}"
-    
-        # 2- Data extraction to use in naming the snapshot files
-        time = raw_data['time']
-        license_plate = raw_data['License Plate']
-        device_name = raw_data['device']
-        coordinates = {
-            'x1':raw_data['coordinate_x1'],
-            'y1':raw_data['coordinate_y1'],
-            'x2':raw_data['coordinate_x2'],
-            'y2':raw_data['coordinate_y2']
+            headers = {
+            "Authorization": f"Bearer {bearer_token}",
+            "Content-Type": "application/json"
         }
-        snapshot_base64 = raw_data['snapshot']
+            
+            base_url = os.getenv("TARGET_URL","http://localhost:3000/parking-session/operatorId/1/floorId/1")
+            print("operator id:",operatorId)
+            print("floor id:",floorId)
+            print("raw data", raw_data)
 
-        # 3 - helper function : Image compresion , overlay   
-        processed_image = snapshot_processing( coordinates, snapshot_base64)
-
-        # 4 - save and  convert the processed image into base64 again to send to NESTJS
-        base64_processed_image = save_and_apply_overlay(time, license_plate, device_name, processed_image)
-
-        # 5 - Update the base64 value  with the processed  image one
-        raw_data['snapshot'] = base64_processed_image 
-
-        # 6 - send POST request to NESTJS
-        response = requests.post(target_url,json=raw_data, headers=headers)
-        print(response.content)
-
-        #7 - Store the data into the database
-        db_response = save_parking_event_to_db(operatorId, floorId,raw_data)
-        if db_response["status"] == "error":
-            return jsonify({"error": db_response["message"]}), 500
+            # 1 -Generate target url to NESTJS
+            target_url = f"{base_url}/parking-session/operatorId/{operatorId}/floorId/{floorId}"
         
-        return response.content, response.status_code
-        # return "OK"
-    except requests.exceptions.Timeout:
-        # Handle timeout errors
-        return jsonify({"error": "Request to target server timed out"}), 504
-    
-    except requests.exceptions.ConnectionError:
-        # Handle connection errors
-        return jsonify({"error": "Failed to connect to target server"}), 502
-    
-    except requests.exceptions.RequestException as e:
-        # Catch other request-related exceptions
-        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+            # 2- Data extraction to use in naming the snapshot files
+            time = raw_data['time']
+            license_plate = raw_data['License Plate']
+            device_name = raw_data['device']
+            coordinates = {
+                'x1':raw_data['coordinate_x1'],
+                'y1':raw_data['coordinate_y1'],
+                'x2':raw_data['coordinate_x2'],
+                'y2':raw_data['coordinate_y2']
+            }
+            snapshot_base64 = raw_data['snapshot']
 
-    except Exception as e:
-        # Handle unexpected server-side errors
-        return jsonify({"error": "Internal server error", "details": str(e)}), 500
+            # 3 - helper function : Image compresion , overlay   
+            processed_image = snapshot_processing( coordinates, snapshot_base64)
 
+            # 4 - save and  convert the processed image into base64 again to send to NESTJS
+            base64_processed_image = save_and_apply_overlay(time, license_plate, device_name, processed_image)
 
+            # 5 - Update the base64 value  with the processed  image one
+            raw_data['snapshot'] = base64_processed_image 
+
+            # 6 - send POST request to NESTJS
+            response = requests.post(target_url,json=raw_data, headers=headers)
+            print(response.content)
+
+            #7 - Store the data into the database
+            db_response = save_parking_event_to_db(operatorId, floorId,raw_data)
+            if db_response["status"] == "error":
+                return jsonify({"error": db_response["message"]}), 500
+            
+            return response.content, response.status_code
+            # return "OK"
+        except requests.exceptions.Timeout:
+            # Handle timeout errors
+            return jsonify({"error": "Request to target server timed out"}), 504
+        
+        except requests.exceptions.ConnectionError:
+            # Handle connection errors
+            return jsonify({"error": "Failed to connect to target server"}), 502
+        
+        except requests.exceptions.RequestException as e:
+            # Catch other request-related exceptions
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+        except Exception as e:
+            # Handle unexpected server-side errors
+            return jsonify({"error": "Internal server error", "details": str(e)}), 500
+    return api
 
 def snapshot_processing( coordinates, snapshot_base64):
     print("snapshot processing ok")
@@ -108,18 +105,18 @@ def snapshot_processing( coordinates, snapshot_base64):
 
 
 def save_and_apply_overlay(time, license_plate, device_name, img):
-     # overlay text formatting
+    # overlay text formatting
 
     dt_object = datetime.strptime(time,'%Y-%m-%d %H:%M:%S')
     formatted_time = dt_object.strftime('%Y%m%d_%H%M%S')
     folder_name = dt_object.strftime('%Y%m%d')
     file_name = f"{formatted_time}_{license_plate}.jpg"
 
-    # creating dedicated snapshot folder
+        # creating dedicated snapshot folder
     snapshot_dir = os.path.join(os.getcwd(), 'snapshot')
     os.makedirs(snapshot_dir, exist_ok=True)
 
-    # create folder strurcture (date and device folder witihin snapshot)
+        # create folder strurcture (date and device folder witihin snapshot)
     daily_folder_path = os.path.join(snapshot_dir, folder_name)
     device_folder_path = os.path.join(daily_folder_path, device_name)
     os.makedirs(device_folder_path,exist_ok=True)
@@ -161,30 +158,30 @@ def save_parking_event_to_db(operatorId, floorId,raw_data):
     """
     try:
         parking_event = ParkingLog(
-            event=raw_data.get('event'),
-            operator_id = operatorId,
-            floor_id = floorId,
-            device=raw_data.get('device'),
-            time=datetime.strptime(raw_data['time'], '%Y-%m-%d %H:%M:%S'),
-            report_type=raw_data.get('report_type'),
-            resolution_w=raw_data.get('resolution_w'),
-            resolution_h=raw_data.get('resolution_h'),
-            channel=raw_data.get('channel'),
-            bay_name=raw_data.get('bay_name'),
-            bay_id=raw_data.get('bay_id'),
-            occupancy=raw_data.get('occupancy'),
-            duration=raw_data.get('duration'),
-            license_plate=raw_data.get('License Plate'),
-            plate_color=raw_data.get('Plate Color'),
-            vehicle_type=raw_data.get('Vehicle Type'),
-            vehicle_color=raw_data.get('Vehicle Color'),
-            vehicle_brand=raw_data.get('Vehicle Brand'),
-            coordinate_x1=raw_data.get('coordinate_x1'),
-            coordinate_y1=raw_data.get('coordinate_y1'),
-            coordinate_x2=raw_data.get('coordinate_x2'),
-            coordinate_y2=raw_data.get('coordinate_y2'),
-            snapshot=raw_data.get('snapshot')
-        )
+        event=raw_data.get('event'),
+        operator_id = operatorId,
+        floor_id = floorId,
+        device=raw_data.get('device'),
+        time=datetime.strptime(raw_data['time'], '%Y-%m-%d %H:%M:%S'),
+        report_type=raw_data.get('report_type'),
+        resolution_w=raw_data.get('resolution_w'),
+        resolution_h=raw_data.get('resolution_h'),
+        channel=raw_data.get('channel'),
+        bay_name=raw_data.get('bay_name'),
+        bay_id=raw_data.get('bay_id'),
+        occupancy=raw_data.get('occupancy'),
+        duration=raw_data.get('duration'),
+        license_plate=raw_data.get('License Plate'),
+        plate_color=raw_data.get('Plate Color'),
+        vehicle_type=raw_data.get('Vehicle Type'),
+        vehicle_color=raw_data.get('Vehicle Color'),
+        vehicle_brand=raw_data.get('Vehicle Brand'),
+        coordinate_x1=raw_data.get('coordinate_x1'),
+        coordinate_y1=raw_data.get('coordinate_y1'),
+        coordinate_x2=raw_data.get('coordinate_x2'),
+        coordinate_y2=raw_data.get('coordinate_y2'),
+        snapshot=raw_data.get('snapshot')
+            )
         db.session.add(parking_event)
         db.session.commit()
         print("Parking event saved to database.")
